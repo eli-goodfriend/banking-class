5d562fde85bcc0b18af7254f1ce50b0ae63db2b0
Eli Goodfriend <eli.goodfriend@gmail.com>
2017-01-26 17:47:41 -0500

    separate feature extraction and training

--- autoCat.py
+++ autoCat.py
@@ -1,62 +1,30 @@
 """
-automatically categorize transactions based on lookup-labeled transactions
+train model on pre-categorized data
 """
 
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import linear_model
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
+import extractFeatures as ef
 
 # TODO should move to SQL
-datafile = '/home/eli/Dropbox/Code/Narmi/categorized.csv'
+datafile = '/home/eli/Data/Narmi/train_lookupCat.csv'
 transData = pd.read_csv(datafile,encoding='latin-1')
 
-transData = transData[['merchant','date','time','category']]
-
-# make target column
 # TODO also do this earlier
 transData.merchant = transData.merchant.str.upper()
 
-catData = transData[~transData.isUnknown]
-uncatData = transData[transData.isUnknown]
+catData = transData[transData.category >= 0]
+uncatData = transData[transData.category < 0]
 
-# turn time string into number from [0,1) 
-# TODO do this earlier and less jankily
-# TODO use datetime?
-catData['timenum'] = ''
-for index, row in catData.iterrows():
-    timeString = row.time
-    if isinstance(timeString, basestring): # a full time stamp
-        hour = float(timeString[0:2])
-        minute = float(timeString[3:5])
-        second = float(timeString[6:8])
-        timenum = hour/24. + minute/(24.*60.) + second/(24.*60.*60.)
-        catData.set_value(index,'timenum',timenum)
-
-# turn merchant strings into vectors
-# TODO from pre-categorized data or all data
-# TODO what about fuzzy matches: do that now or when defining merchants
-# TODO like make a list of merchants and if a new one is similar enough, change
-#      it to a pre-existing merchant
-# cut one: just do what sklearn tells us to do
-vectorizer = CountVectorizer(min_df=1)
-
-documents = catData.merchant.tolist()
-wordCounts = vectorizer.fit_transform(documents)
-
-# change everything to arrays and fit
-X = wordCounts.toarray()
+X = ef.extract(catData)
 y = catData.category.tolist()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 logreg = linear_model.LogisticRegression()
-logreg.fit(X_train, y_train)
+logreg.fit(X,y)
 
-y_pred = logreg.predict(X_test)
-acc = metrics.accuracy_score(y_test, y_pred) # unsurprisingly, is very good
+acc = logreg.score(X,y)
     
-print "Accuracy of test set is " + str(acc*100) + "%"
+print "Accuracy of pre-categorized train set is " + str(acc*100) + "%"
 
 # predict categories for the as-yet uncategorized data
 #newDocuments = uncatData.merchant.tolist()
---
+++ extractFeatures.py
@@ -0,0 +1,39 @@
+"""
+extract features using sklearn vectorizer
+input: df cleaned transaction dataframe
+output: X an array of features
+"""
+from sklearn.feature_extraction.text import CountVectorizer
+
+def extract(df):
+    # turn time string into number from [0,1) 
+    # TODO do this earlier and less jankily
+    # TODO use datetime?
+    # TODO not actually including this yet
+#    df['timenum'] = None
+#    for index, row in df.iterrows():
+#        timeString = row.time
+#        if isinstance(timeString, basestring): # a full time stamp
+#            hour = float(timeString[0:2])
+#            minute = float(timeString[3:5])
+#            second = float(timeString[6:8])
+#            timeFeat = hour/24. + minute/(24.*60.) + second/(24.*60.*60.)
+#            df.set_value(index,'timenum',timeFeat)
+            
+    # turn merchant strings into vectors
+    # TODO from pre-categorized data or all data
+    # TODO what about fuzzy matches: do that now or when defining merchants
+    # TODO like make a list of merchants and if a new one is similar enough, change
+    #      it to a pre-existing merchant
+    # cut one: just do what sklearn tells us to do
+    vectorizer = CountVectorizer(min_df=1)
+    
+    documents = df.merchant.tolist()
+    wordCounts = vectorizer.fit_transform(documents)
+    
+    # change everything to arrays and fit
+    X = wordCounts.toarray()
+    
+    return X
+            
+            
